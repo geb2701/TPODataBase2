aae3bee8bde0460b25221509cc7ca44a08678f98
Federico Alvarez Rouco <fedealvarou@gmail.com>
2025-06-16 17:01:00 -0300

    Agregado campo equipos a Empresa

--- Dtos/Empresa/EmpresaCreateDto.py
+++ Dtos/Empresa/EmpresaCreateDto.py
@@ -1,4 +1,7 @@
+# EmpresaCreateDto.py
 from pydantic import BaseModel
+from typing import List, Optional
 
 class EmpresaCreateDto(BaseModel):
     nombre: str
+    equipos: Optional[List[str]] = []
--- Dtos/Empresa/EmpresaUpdateDto.py
+++ Dtos/Empresa/EmpresaUpdateDto.py
@@ -1,5 +1,7 @@
+# EmpresaUpdateDto.py
 from pydantic import BaseModel
 from typing import List, Optional
 
 class EmpresaUpdateDto(BaseModel):
     nombre: Optional[str]
+    equipos: Optional[List[str]] = None
